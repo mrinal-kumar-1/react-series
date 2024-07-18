7052b8afb64a78b2d0822e9995a0b4a32f64a4ba
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-07-18 12:32:15 +0530

    Beginning with React using Vite

---
+++ viteReactProject/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <h2>Hello
+      how are you doing ?
+    </h2>
+  )
+}
+
+export default App
