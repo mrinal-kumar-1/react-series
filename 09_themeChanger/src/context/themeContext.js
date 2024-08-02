702e7af841bf92d97d1ab52cf2a21ee86cb8befd
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-08-02 17:14:59 +0530

    Light Mode, Dark Mode theme changer project

---
+++ 09_themeChanger/src/context/themeContext.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { createContext, useContext } from "react";
+
+export const ThemeContext = createContext({
+    themeMode: "light",
+    darkTheme : () => {
+        
+    },
+    lightTheme : () => {
+
+    }
+})
+
+export const ThemeContextProvider = ThemeContext.Provider;
+
+export default function useTheme(){
+    return (
+        useContext(ThemeContext)
+    )
+}
+
+
+
