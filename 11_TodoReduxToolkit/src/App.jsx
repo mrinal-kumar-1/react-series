c1f47c7b3ba99c193f25e6750a9f91d50378669f
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-08-19 12:59:21 +0530

    Redux Todo Project

--- 11_TodoReduxToolkit/src/App.jsx
+++ 11_TodoReduxToolkit/src/App.jsx
@@ -1,13 +1,17 @@
+import React from 'react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import AddTodo from './Components/AddTodo'
+import Todos from './Components/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <h1>Hello Guys</h1>
+    <>
+      <AddTodo></AddTodo>
+      <Todos></Todos>
+    </>
   )
 }
 
--- 11_TodoReduxToolkit/src/TodoSlice/slice.js
+++ 11_TodoReduxToolkit/src/TodoSlice/slice.js
@@ -3,29 +3,29 @@ import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = {
   todos:  [
-          {id: 1, message:"", completed: false}
+          {id: 1, message:"Hello Guys", completed: false}
     ]
 }
 
 export const todoSlice = createSlice(
     {
-        name: todo,
+        name: 'todo',
         initialState,
         reducers: {
             addTodo: (state, action) => {
                    const todo = {
                     id: nanoid,
-                    message: action.payload.message,
+                    message: action.payload,
                     completed: false
                    }
 
                    state.todos.push(todo)
             },
             removeTodo: (state, action) => {
-                state.todos = state.todos.filter((val) => val.id != action.payload.id)
+                state.todos = state.todos.filter((val) => val.id != action.payload)
             },
             updateTodo: (state, action) => {
-                state.todos = state.todos.map((val) => (val.id == action.payload.id ? val.message = action.payload.message : val))
+                state.todos = state.todos.map((val) => (val.id == action.payload ? val.message = action.payload : val))
             }
         }
 
