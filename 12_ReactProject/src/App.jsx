e802cab74fe1bc8c70dfcdaadfb1707da2f6fa71
mrinal-kumar-1 <mrinaluser22kumar@gmail.com>
2024-09-03 16:36:02 +0530

    Configure store in this project

--- 12_ReactProject/src/App.jsx
+++ 12_ReactProject/src/App.jsx
@@ -1,12 +1,38 @@
-
+import {useDispatch} from 'react-redux'
+import { useEffect, useState } from 'react'
 import './App.css'
+import authService from './appwrite/auth_services'
+import {login, logout} from './store/authSlice'
+import { Header, Footer } from './components/index'
+import {Outlet} from 'react-router-dom'
 
 function App() {
+ const [loading, setLoading] = useState(true)
+ const dispatch = useDispatch()
+ useEffect(() => {
+   authService.getCurrentUser()
+   .then((userData) => {
+    if(userData){
+      dispatch(login(userData))
+    }
+    else{
+      dispatch(logout())
+    }
+   }).finally(() => setLoading(false))
+ }, [])
 
-  console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <h2>Hi all of You</h2>
-  )
+
+  return !loading ? (
+    <div className=' flex flex-wrap content-between'> Testing
+      <div className='w-full block'>
+        <Header></Header>
+        <main>
+          <Outlet />
+        </main>
+        <Footer></Footer>
+       </div> 
+    </div>
+  ) : null
 }
 
 export default App
